50d9901555f7a2ba01cbbcee445a6a579cd4791c
bousket <bousket@gmail.com>
2017-02-08 08:39:36 +0100

    initial import
    Adding chat bell

---
+++ README.md
@@ -0,0 +1,14 @@
+Chat Bell
+=========
+
+Chat Bell is a tiny mod ringing a bell on player's name quote in chat.
+The functionality can be enabled/disabled by /chatbell command.
+Players preferences are saved on disk in <mod place>/chatbell by default
+
+Mod dependencies: none
+
+License
+=======
+
+Copyright (C) 2017 Bousket
+Chat Bell code is licensed under the GNU LGPLv3+.
---
+++ init.lua
@@ -0,0 +1,29 @@
+-- Chat Bell - Mod for minetest
+-- Copyright (C) 2017 Bousket
+--
+-- ChatBell is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Lesser General Public License as 
+-- published by the Free Software Foundation, either version 3 of the
+-- License, or (at your option) any later version.
+--
+-- ChatBell is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU Lesser General Public
+-- License along with ChatBell.  If not, see <http://www.gnu.org/licenses/>.
+--
+-------------------------------------------------------------------------------
+--
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+-- !!! This mod is temporary and should be implemented client side. !!
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+--
+-- Feature:
+--  * Emits a sound on my name
+--  * /chatbell Enable/Disable this ability
+--  * Save players preferences on disk
+
+
+dofile(minetest.get_modpath(minetest.get_current_modname()).."/chatbell.lua")
